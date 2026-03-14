f1b1574c3355d429ac1f41c9008ae63f79558d4a
MarkoKliska <021markokliska@gmail.com>
2026-03-14 15:12:41 +0100

    Added logs to GetAllInvoicesQueryHandler

--- backend/src/Autoservice.Application/Autoservice.Application/Features/Invoice/GetAllInvoices/GetAllInvoicesQueryHandler.cs
+++ backend/src/Autoservice.Application/Autoservice.Application/Features/Invoice/GetAllInvoices/GetAllInvoicesQueryHandler.cs
@@ -1,18 +1,23 @@
 ﻿using Autoservice.Application.DTOs.Common;
 using Autoservice.Application.DTOs.ServiceInvoice;
+using Autoservice.Application.Interfaces;
 using Autoservice.Domain.Repositories;
 using MediatR;
+
 
 namespace Autoservice.Application.Features.Invoice.GetAllInvoices;
 
 public sealed class GetAllInvoicesQueryHandler(
-    IServiceInvoiceRepository invoices
+    IServiceInvoiceRepository invoices,
+    IEventLogger logger
 ) : IRequestHandler<GetAllInvoicesQuery, Result<IEnumerable<ServiceInvoiceResponseDto>>>
 {
     public async Task<Result<IEnumerable<ServiceInvoiceResponseDto>>> Handle(
         GetAllInvoicesQuery request,
         CancellationToken ct)
     {
+        logger.Info("Manager requested all service invoices.");
+
         var all = await invoices.GetAllAsync(ct);
 
         var response = all.Select(i => new ServiceInvoiceResponseDto
@@ -24,7 +29,9 @@ public sealed class GetAllInvoicesQueryHandler(
             VehicleLicensePlate = i.Vehicle.LicensePlate,
             VehicleBrand = i.Vehicle.Brand,
             VehicleModel = i.Vehicle.Model
-        });
+        }).ToList();
+
+        logger.Info($"Returned {response.Count} invoice(s).");
 
         return Result<IEnumerable<ServiceInvoiceResponseDto>>.Success(response);
     }
